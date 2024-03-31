ece18e50d32b486ef2260f7b6c209f18d4eb6d28
John Cameron <john.n.cameron@gmail.com>
2024-03-31 10:38:33 +1000

    initial ui

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,19 @@
+import MobileNav from "@/components/shared/MobileNav";
+import SideBar from "@/components/shared/SideBar";
+import { ClerkProvider } from "@clerk/nextjs";
+import React, { ReactNode } from "react";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className="root">
+      <SideBar />
+      <MobileNav />
+      <div className="root-container">
+        <div className="wrapper"></div>
+      </div>
+      {children}
+    </main>
+  );
+};
+
+export default Layout;
